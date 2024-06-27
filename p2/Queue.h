995c61518bf2107ffdc713195aff87257e6d6a49
VShawFluenta <Victoria.j.shaw.47@gmail.com>
2024-06-26 23:42:09 -0700

    can't seem to find the queue library

---
+++ p2/Queue.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h> 
+#include <unistd.h>
+#include <errno.h>
+
+customer * pop(){}
+
+customer * peek(){
+}
+
+void push(customer * cust){}
+
