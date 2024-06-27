abdce17e9811a2d81af23b67858cc295bfac5c8b
VShawFluenta <Victoria.j.shaw.47@gmail.com>
2024-06-26 22:52:01 -0700

    made queue

--- p2/Queue.c
+++ p2/Queue.c
@@ -1,1 +1,37 @@
-// adding text to test if I changes profile thigns correctly
+//Queue to hold customers, by Victoria Shaw
+#include <stddef.h>
+#include <stdio.h> 
+typedef struct _customer{
+	int id;
+	int arrivalTime;
+	int serviceTime; //e.g., service time
+    struct _customer *next; 
+}customer;
+
+customer  * tail = NULL; //points to the last node,
+customer * head = NULL; 
+int quantity =0; 
+
+customer * pop(){
+    customer * top = head; 
+    head = head -> next; 
+    quantity --; 
+    return top; 
+}
+
+customer * peek(){
+    customer * top = head; 
+    return top; 
+}
+
+void push(customer * cust){
+    if(head == NULL){
+        head = cust; 
+        tail = cust; 
+    } else{
+        tail->next = cust; 
+        tail = cust;
+    }
+    quantity ++; 
+
+}
--- p2/main.c
+++ p2/main.c
@@ -1,4 +1,40 @@
 //Victoria Shaw
 //V01020304 Assignment 2
 //CSC 360
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h> 
+#include <unistd.h>
+#include <errno.h>
+#include <Queue.h> 
 
+int customersToProcess =0; 
+
+
+void clerk(){
+    while(customersToProcess !=0){//might need a mutex on this
+
+    }
+}
+void loadCustomers(){//don't forget to check for invalid times 
+}
+
+int main(){
+    double sumBusinessWait=0; 
+    double sumEconomyWait =0; 
+    pthread_t threadArray[5]; 
+    int rc; 
+
+    for(int i =0; i < 5; i ++){
+        if ((rc = pthread_create(&threadArray[i], NULL, clerk, ))) {
+			fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
+			return EXIT_FAILURE;
+		}
+    }
+
+    for (int i = 0; i < 5; ++i) {
+		pthread_join(threadArray[i], NULL);
+	}
+
+}
